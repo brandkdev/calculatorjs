29353e48dc49802fc037fefc289770355c92ab34
willbraken <brandon@willbraken.com>
2022-07-20 21:07:06 -0500

    add HTML, CSS & JS

---
+++ script.js
@@ -0,0 +1,31 @@
+// A program for an HTML operated calculator!
+
+function operate(a, operator, b) {
+    if(operator === '+') {
+        return add(a, b);
+    } else if(operator === '-') {
+        return subtract(a, b);
+    } else if(operator === '*') {
+        return multiply(a, b);
+    } else if(operator === '/') {
+        return divide(a, b);
+    } else {
+        console.log('Please enter a valid expression');
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
